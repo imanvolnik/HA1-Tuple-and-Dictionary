e152cf8842c9ca8373153187d7d86fc9dcf6314d
Iman <99523900+imanvolnik@users.noreply.github.com>
2024-01-26 11:41:27 +0600

    Create laptop.py

---
+++ laptop.py
@@ -0,0 +1,12 @@
+laptop = {"brand": "dell", "model": "alienware", "year": 2010}
+
+# 1. Print the brand value of the dictionary
+print(laptop["brand"])
+
+# 2. Add new information (key: value)by using a boolean data type as value and home as key: This laptop is at home now, so "home": True
+laptop["home"] = True
+print(laptop)
+
+#3. Modify the value of the key year to 2019
+laptop["year"] = 2019
+print(laptop)
